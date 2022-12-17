bd2366d5fb87292b94efec4253c173adedf092e5
hovanloi <hovanloidev@gmail.com>
2022-12-17 22:37:12 +0000

    slider work list

--- src/modules/Home/Content/Slider/Slider.jsx
+++ src/modules/Home/Content/Slider/Slider.jsx
@@ -3,13 +3,22 @@ import Style from "./slider.module.css";
 import cn from "classnames";
 import { Link } from "react-router-dom";
 import item1 from "../../../../imgs/slider/item1.jpg";
-const Slider = () => {
+import Slider from "react-slick";
+
+const MySlider = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 3,
+  };
   return (
     <>
       <div className={Style.slider}>
         <div className={cn("container", Style.sliderContainer)}>
           <h3 className={Style.heading}>Popular professional services</h3>
-          <div className={Style.list}>
+          <Slider {...settings} className={Style.list}>
             <div className={Style.item}>
               <Link className={Style.link}>
                 <img src={item1} alt="" className={Style.img} />
@@ -19,11 +28,81 @@ const Slider = () => {
                 </div>
               </Link>
             </div>
-          </div>
+
+            <div className={Style.item}>
+              <Link className={Style.link}>
+                <img src={item1} alt="" className={Style.img} />
+                <div className={Style.desc}>
+                  <p className={Style.text}>Build your brand</p>
+                  <h4 className={Style.itemHeading}>Logo Design</h4>
+                </div>
+              </Link>
+            </div>
+
+            <div className={Style.item}>
+              <Link className={Style.link}>
+                <img src={item1} alt="" className={Style.img} />
+                <div className={Style.desc}>
+                  <p className={Style.text}>Build your brand</p>
+                  <h4 className={Style.itemHeading}>Logo Design</h4>
+                </div>
+              </Link>
+            </div>
+
+            <div className={Style.item}>
+              <Link className={Style.link}>
+                <img src={item1} alt="" className={Style.img} />
+                <div className={Style.desc}>
+                  <p className={Style.text}>Build your brand</p>
+                  <h4 className={Style.itemHeading}>Logo Design</h4>
+                </div>
+              </Link>
+            </div>
+
+            <div className={Style.item}>
+              <Link className={Style.link}>
+                <img src={item1} alt="" className={Style.img} />
+                <div className={Style.desc}>
+                  <p className={Style.text}>Build your brand</p>
+                  <h4 className={Style.itemHeading}>Logo Design</h4>
+                </div>
+              </Link>
+            </div>
+
+            <div className={Style.item}>
+              <Link className={Style.link}>
+                <img src={item1} alt="" className={Style.img} />
+                <div className={Style.desc}>
+                  <p className={Style.text}>Build your brand</p>
+                  <h4 className={Style.itemHeading}>Logo Design</h4>
+                </div>
+              </Link>
+            </div>
+
+            <div className={Style.item}>
+              <Link className={Style.link}>
+                <img src={item1} alt="" className={Style.img} />
+                <div className={Style.desc}>
+                  <p className={Style.text}>Build your brand</p>
+                  <h4 className={Style.itemHeading}>Logo Design</h4>
+                </div>
+              </Link>
+            </div>
+
+            <div className={Style.item}>
+              <Link className={Style.link}>
+                <img src={item1} alt="" className={Style.img} />
+                <div className={Style.desc}>
+                  <p className={Style.text}>Build your brand</p>
+                  <h4 className={Style.itemHeading}>Logo Design</h4>
+                </div>
+              </Link>
+            </div>
+          </Slider>
         </div>
       </div>
     </>
   );
 };
 
-export default Slider;
+export default MySlider;
